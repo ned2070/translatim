68cdaa943aaa5ed61bf23e629e46a0fdedc5d4f3
ned2070te <ned2070te@gmail.com>
2023-11-02 11:08:28 +0000

    server

--- server/server.js
+++ server/server.js
@@ -8,4 +8,16 @@ const axios = require("axios");
 
 app.get("/", (_, response) => response.json("Root route for translatim."));
 
+app.get("/translate", async (request, response) => {
+  // const word = request.query.word;
+  // const from = request.query.from;
+  // const to = request.query.to;
+  // const {word, from, to } =  request.query;
+
+  const API = `https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=en|es`;
+
+  const res = await axios.get(API);
+
+  response.json(res, data);
+});
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
